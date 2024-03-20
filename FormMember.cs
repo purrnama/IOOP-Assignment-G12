b6ea4a51951564ba45388e699222145262512ab1
ahmedASM1 <alshadadi90@gmail.com>
2024-03-20 14:44:23 +0800

    feat: member form

---
+++ FormMember.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IOOPAssignment_G12
+{
+    public partial class FormMember: Form
+    {
+        public FormMember()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string id = txtUsername.Text;
+            string pass = txtPassword.Text;
+
+            if (id == "member" && pass == "12345")
+            {
+                this.Hide();
+
+                FormMember f = new FormMember();
+                f.Show();
+            }
+            else
+            {
+                MessageBox.Show("Password or Username is incorrect...");
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+        }
+    }
+}
