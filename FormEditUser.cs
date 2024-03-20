d3368212be681a1db8714c42f956951a0916e7be
SmolPadok <qay.yaz19@gmail.com>
2024-03-20 14:39:56 +0800

    feat: edit user form

---
+++ FormEditUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IOOPAssignment_G12
+{
+    public partial class frmEditUser : Form
+    {
+        User editUser;
+        public frmEditUser()
+        {
+            InitializeComponent();
+        }
+        public frmEditUser(string username)
+        {
+            InitializeComponent();
+            editUser = new User(username);
+            User.ViewProfile(editUser);
+            txtBoxUsername.Text = editUser.Username;
+            txtBoxPassword.Text = editUser.Password;
+            txtBoxFullName.Text = editUser.FullName;
+            txtBoxEmail.Text = editUser.Email;
+            txtBoxPhone.Text = editUser.Phone;
+        }
+    }
+}
